7d42a53260a056aac5489d2e78b60c13becfd1b2
antb4 <ant5692@gmail.com>
2023-09-20 01:04:43 -0700

    added e-mail functionality and intro styling

--- src/Sections/Contact/contact.js
+++ src/Sections/Contact/contact.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import github from "../../assets/github.png";
 import linkedin from "../../assets/linkedin.png";
 
+//Email.js
+import emailjs from '@emailjs/browser';
+
  const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_a04ol8h', 'template_70d3dnk', form.current, 'sI8rkMrdCFIX5kopR')
+      .then((result) => {
+        console.log(result.text);
+        let response = document.querySelector(".email-response");
+        console.log(response);
+        if (response.classList.contains("hidden")) {
+          response.classList.remove("hidden");
+        };
+        e.target.reset();
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
   return (
     <div className="contact-container">
     <section id="contact">
       <h2 className="contact-title">Contact Me</h2>
       <p className="contact-description">Send me a message, let's do something awesome together!</p>
-      <form className="contact-form">
-        <input type="text" className="contact-name" placeholder="Your Name" />
-        <input type="email" className="contact-email" placeholder='Your Email' />
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
+        <input type="text" className="contact-name" placeholder="Your Name" name="your_name" />
+        <input type="email" className="contact-email" placeholder='Your Email' name="your_email" />
         <textarea name="message" className="contact-message" rows="5" placeholder='Your Message'></textarea>
         <button className="contact-submit" type="submit" value='Send'>Submit</button>
+        <p className="email-response hidden">Thanks for the e-mail! I'll get back to you asap!</p>
         <div className="contact-links">
           <a href="https://github.com/anthony-balfour"><img className="contact-icon" src={github} alt="github icon" /></a>
           <a href="https://www.linkedin.com/in/anthonybalfourjr/"><img className="contact-icon" src={linkedin} alt ="linekdin icon" /></a>
